81525fa43c0d5f74ad1b2ff56e209d920e732a75
ValeskaSK <va.guardia@duocuc.cl>
2026-01-17 17:03:21 -0300

    chore: limpiar app.js y dejar middlewares globales

--- app.js
+++ app.js
@@ -3,27 +3,34 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { rateLimit } = require('express-rate-limit');
 const errorHandler = require('./src/api/middlewares/errorHandler.middleware');
-const geoRoutes = require('./src/api/components/geo/geo.routes');
 const ApiError = require('./src/utils/ApiError');
 
 const app = express();
 
+// Middlewares Globales
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+// Limitador de peticiones (15 min - 100 peticiones)
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
+  message: 'Demasiadas peticiones desde esta IP, por favor intente más tarde.'
 });
 app.use(limiter);
 
-app.use('/api/v1/geo', geoRoutes);
+/**
+ * Rutas: Aquí montarás los routers de cada microservicio
+ */
+// app.use('/api/v1/auth', authRoutes);
 
+// Manejo de rutas no encontradas (404)
 app.use((req, res, next) => {
-  next(new ApiError(404, 'Not Found'));
+  next(new ApiError(404, 'Ruta no encontrada'));
 });
 
+// Middleware Global de Errores
 app.use(errorHandler);
 
 module.exports = app;
