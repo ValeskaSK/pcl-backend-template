0c45c700fdd462684ff9c0732031066176dac278
ValeskaSK <va.guardia@duocuc.cl>
2026-01-18 10:43:04 -0300

    refactor: limpiar template base y dejar solo infraestructura

--- src/api/middlewares/checkJwt.middleware.js
+++
@@ -1,9 +0,0 @@
-const { auth } = require('express-oauth2-jwt-bearer');
-const config = require('../../config');
-
-const checkJwt = auth({
-  audience: config.auth0.audience,
-  issuerBaseURL: config.auth0.issuerUrl,
-});
-
-module.exports = checkJwt;
